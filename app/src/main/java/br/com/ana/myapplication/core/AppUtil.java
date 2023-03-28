45649e8327fa0b3518c65e4a067f3e6062198ee6
Ana Lidia Muxfeldt <anaamux@gmail.com>
2023-03-28 16:04:48 -0300

    Melhoramento da implementação de data e hora na tela Splash.

--- app/src/main/java/br/com/ana/myapplication/core/AppUtil.java
+++ app/src/main/java/br/com/ana/myapplication/core/AppUtil.java
@@ -2,7 +2,10 @@ package br.com.ana.myapplication.core;
 
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AppUtil {
 
@@ -20,23 +23,12 @@ public class AppUtil {
      * @return devolve a data atual.
      */
     public static String getDataAtual(){
+        String dataAtual= "00/00/00";
 
-        String dia, mes, ano;
-        String dataAtual = "28/03/2023";
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date data = new Date();
 
-        try {
-            Calendar calendar = Calendar.getInstance();
-            dia = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
-            mes = String.valueOf(calendar.get(Calendar.MONTH) + 1);
-            ano = String.valueOf(calendar.get(Calendar.YEAR));
-
-            dia = (Calendar.DAY_OF_MONTH < 10) ? "0"+ dia : dia;
-            int mesAtual = (Calendar.MONTH) + 1;
-            mes = (mesAtual < 10) ? "0" + mes : mes;
-
-        } catch (Exception e) {
-
-        }
+        dataAtual = dateFormat.format(data);
 
         return dataAtual;
     }
@@ -46,25 +38,13 @@ public class AppUtil {
      * @return devolve a hora atual
      */
     public static String getHoraAtual(){
+        String horaAtual= "00/00/00";
 
-        String horaAtual = "00:00:00";
-        String hora, minuto, segundo;
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        Date data = new Date();
 
-        try {
-            Calendar calendar= Calendar.getInstance();
-            int iHora= calendar.get(Calendar.HOUR_OF_DAY);
-            int iMinuto=calendar.get(Calendar.MINUTE);
-            int iSegundo=calendar.get(Calendar.SECOND);
+        horaAtual = dateFormat.format(data);
 
-            hora = (iHora<= 9) ? "0"+iHora : Integer.toString(iHora);
-            minuto = (iMinuto<= 9) ? "0"+iMinuto : Integer.toString(iMinuto);
-            segundo = (iSegundo<= 9) ? "0"+iSegundo : Integer.toString(iSegundo);
-
-            horaAtual = hora+":"+minuto+":"+segundo;
-
-        } catch (Exception e) {
-
-        }
 
         return horaAtual;
     }
